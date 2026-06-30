3b863f861589b90260e190a0b68975c841f721ab
Vitor <v.luz@unesp.br>
2026-06-30 20:56:51 -0300

    arrumado erro com classe pessoa

--- springtutorial/src/main/java/br/unesp/rc/springtutorial/entity/Pessoa.java
+++ springtutorial/src/main/java/br/unesp/rc/springtutorial/entity/Pessoa.java
@@ -3,18 +3,27 @@ package br.unesp.rc.springtutorial.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+@MappedSuperclass
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
 
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idPessoa;
+    
     private String nome;
     private List<Endereco> endereco;
     private Acesso acesso;
