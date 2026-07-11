74bfd16e1ed757f8b029ee7f05f543fad6528ba5
Vitor <v.luz@unesp.br>
2026-07-11 18:07:10 -0300

    inicial

---
+++ springtutorial/springtutorialmongodb/src/main/java/br/unesp/br/springtutorialmongodb/SpringtutorialmongodbApplication.java
@@ -0,0 +1,13 @@
+package br.unesp.br.springtutorialmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtutorialmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtutorialmongodbApplication.class, args);
+	}
+
+}
